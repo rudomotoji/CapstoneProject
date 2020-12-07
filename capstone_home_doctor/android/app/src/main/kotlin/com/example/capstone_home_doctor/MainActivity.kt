68c3e5e9f3bca7c52d2025e0497fadb3ad418da1
vuong <dinnhvuongnhoc123.vn57@gmail.com>
2020-12-07 12:52:55 +0700

    create project

---
+++ capstone_home_doctor/android/app/src/main/kotlin/com/example/capstone_home_doctor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.capstone_home_doctor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ capstone_home_doctor/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'common/constant/evn.dart';
+
+void main() {
+  runApp(MyApp(env: EnvValue.development));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  final Env env;
+
+  MyApp({Key key, @required this.env}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
