cd1f9658af7b8a6b2c092bd9b0d81908d8128b58
vuong <dinnhvuongnhoc123.vn57@gmail.com>
2021-03-14 13:15:26 +0700

    thêm ui lịch tái khám

--- capstone_home_doctor/lib/features/schedule/views/schedule_view.dart
+++ capstone_home_doctor/lib/features/schedule/views/schedule_view.dart
@@ -12,6 +12,7 @@ import 'package:capstone_home_doctor/services/authen_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 final AuthenticateHelper _authenticateHelper = AuthenticateHelper();
 
@@ -22,7 +23,8 @@ class ScheduleView extends StatefulWidget {
   }
 }
 
-class _ScheduleView extends State<ScheduleView> with WidgetsBindingObserver {
+class _ScheduleView extends State<ScheduleView>
+    with WidgetsBindingObserver, TickerProviderStateMixin {
   //
   DateValidator _dateValidator = DateValidator();
   PrescriptionDTO _currentPrescription = PrescriptionDTO();
@@ -34,17 +36,91 @@ class _ScheduleView extends State<ScheduleView> with WidgetsBindingObserver {
   //
   int _patientId = 0;
 
+  //use for calendar
+  Map<DateTime, List> _events;
+  List _selectedEvents;
+  AnimationController _animationController;
+  CalendarController _calendarController;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _getPatientId();
     _prescriptionListBloc = BlocProvider.of(context);
+
+    final _selectedDay = DateTime.now();
+
+    _events = {
+      _selectedDay.subtract(Duration(days: 30)): [
+        'Event A0',
+        'Event B0',
+        'Event C0'
+      ],
+      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
+      _selectedDay.subtract(Duration(days: 20)): [
+        'Event A2',
+        'Event B2',
+        'Event C2',
+        'Event D2'
+      ],
+      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
+      _selectedDay.subtract(Duration(days: 10)): [
+        'Event A4',
+        'Event B4',
+        'Event C4'
+      ],
+      _selectedDay.subtract(Duration(days: 4)): [
+        'Event A5',
+        'Event B5',
+        'Event C5'
+      ],
+      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+      _selectedDay.add(Duration(days: 1)): [
+        'Event A8',
+        'Event B8',
+        'Event C8',
+        'Event D8'
+      ],
+      _selectedDay.add(Duration(days: 3)):
+          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+      _selectedDay.add(Duration(days: 7)): [
+        'Event A10',
+        'Event B10',
+        'Event C10'
+      ],
+      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
+      _selectedDay.add(Duration(days: 17)): [
+        'Event A12',
+        'Event B12',
+        'Event C12',
+        'Event D12'
+      ],
+      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
+      _selectedDay.add(Duration(days: 26)): [
+        'Event A14',
+        'Event B14',
+        'Event C14'
+      ],
+    };
+
+    _selectedEvents = _events[_selectedDay] ?? [];
+    _calendarController = CalendarController();
+
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 400),
+      vsync: this,
+    );
+
+    _animationController.forward();
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
+    _animationController.dispose();
+    _calendarController.dispose();
     super.dispose();
   }
 
@@ -133,8 +209,16 @@ class _ScheduleView extends State<ScheduleView> with WidgetsBindingObserver {
                 child: TabBarView(
                   children: <Widget>[
                     _getMedicineSchedule(),
-                    Container(
-                      child: Text('2'),
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        // Switch out 2 lines below to play with TableCalendar's settings
+                        //-----------------------
+                        _buildTableCalendar(),
+                        // _buildTableCalendarWithBuilders(),
+                        const SizedBox(height: 8.0),
+                        Expanded(child: _buildEventList()),
+                      ],
                     ),
                     Container(
                       child: Text('3'),
@@ -146,6 +230,50 @@ class _ScheduleView extends State<ScheduleView> with WidgetsBindingObserver {
           ),
         ),
       ),
+    );
+  }
+
+  // Simple TableCalendar configuration (using Styles)
+  Widget _buildTableCalendar() {
+    return TableCalendar(
+      calendarController: _calendarController,
+      events: _events,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      calendarStyle: CalendarStyle(
+        selectedColor: DefaultTheme.CHIP_BLUE,
+        todayColor: DefaultTheme.BLUE_REFERENCE,
+        markersColor: Colors.brown[700],
+        outsideDaysVisible: false,
+      ),
+      headerStyle: HeaderStyle(
+        formatButtonTextStyle:
+            TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+        formatButtonDecoration: BoxDecoration(
+          color: Colors.deepOrange[400],
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+      onDaySelected: _onDaySelected,
+      onVisibleDaysChanged: _onVisibleDaysChanged,
+    );
+  }
+
+  Widget _buildEventList() {
+    return ListView(
+      children: _selectedEvents
+          .map((event) => Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.8),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                child: ListTile(
+                  title: Text(event.toString()),
+                  onTap: () => print('$event tapped!'),
+                ),
+              ))
+          .toList(),
     );
   }
 
@@ -810,6 +938,17 @@ class _ScheduleView extends State<ScheduleView> with WidgetsBindingObserver {
     });
     _prescriptionListBloc
         .add(PrescriptionListEventsetPatientId(patientId: _patientId));
+  }
+
+  void _onDaySelected(DateTime day, List events, List holidays) {
+    setState(() {
+      _selectedEvents = events;
+    });
+  }
+
+  void _onVisibleDaysChanged(
+      DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onVisibleDaysChanged');
   }
 }
 
