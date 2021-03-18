9153643331e9f84aabe9de3e9a41e28b841cfa73
vuong <dinnhvuongnhoc123.vn57@gmail.com>
2021-03-18 17:13:48 +0700

    đổi tiền từ số sang chữ

--- capstone_home_doctor/lib/commons/utils/convert_utils.dart
+++ capstone_home_doctor/lib/commons/utils/convert_utils.dart
@@ -23,7 +23,7 @@ class ConvertUtils {
     var so = 0;
     var KetQua = "";
     var tmp = "";
-    var ViTri = List();
+    var ViTri = [0, 0, 0, 0, 0, 0];
     if (int.parse(soTien) < 0) return "Số tiền âm !";
     if (int.parse(soTien) == 0) return "Không đồng !";
     if (int.parse(soTien) > 0) {
@@ -31,25 +31,26 @@ class ConvertUtils {
     } else {
       so = -int.parse(soTien);
     }
-    if (soTien > 8999999999999999) {
+    if (so > 8999999999999999) {
       //soTien = 0;
       return "Số quá lớn!";
     }
+
     ViTri[5] = (so / 1000000000000000).floor();
-    if ((ViTri[5]).isNaN) ViTri[5] = "0";
-    so = so - int.parse(ViTri[5].toString()) * 1000000000000000;
+    if ((ViTri[5]).isNaN) ViTri[5] = 0;
+    so = so - ViTri[5] * 1000000000000000;
     ViTri[4] = (so / 1000000000000).floor();
-    if ((ViTri[4]).isNaN) ViTri[4] = "0";
+    if ((ViTri[4]).isNaN) ViTri[4] = 0;
     so = so - int.parse(ViTri[4].toString()) * 1000000000000;
     ViTri[3] = (so / 1000000000).floor();
-    if ((ViTri[3]).isNaN) ViTri[3] = "0";
+    if ((ViTri[3]).isNaN) ViTri[3] = 0;
     so = so - int.parse(ViTri[3].toString()) * 1000000000;
     ViTri[2] = (so / 1000000).floor();
-    if ((ViTri[2]).isNaN) ViTri[2] = "0";
+    if ((ViTri[2]).isNaN) ViTri[2] = 0;
     ViTri[1] = ((so % 1000000) / 1000).floor();
-    if ((ViTri[1]).isNaN) ViTri[1] = "0";
+    if ((ViTri[1]).isNaN) ViTri[1] = 0;
     ViTri[0] = (so % 1000).floor();
-    if ((ViTri[0]).isNaN) ViTri[0] = "0";
+    if ((ViTri[0]).isNaN) ViTri[0] = 0;
     if (ViTri[5] > 0) {
       lan = 5;
     } else if (ViTri[4] > 0) {
@@ -74,18 +75,19 @@ class ConvertUtils {
       KetQua = KetQua.substring(0, KetQua.length - 1);
     }
     KetQua = KetQua.substring(1, 2).toUpperCase() + KetQua.substring(2);
-    return KetQua; //.substring(0, 1);//.toUpperCase();// + KetQua.substring(1);
+    return KetQua +
+        'đồng'; //.substring(0, 1);//.toUpperCase();// + KetQua.substring(1);
   }
 
   //Hàm đọc số có ba chữ số;
-  _docSo3ChuSo(baso) {
+  _docSo3ChuSo(int baso) {
     var tram;
     var chuc;
     var donvi;
     var KetQua = "";
-    tram = int.parse(baso / 100);
-    chuc = int.parse((baso % 100) / 10);
-    donvi = baso % 10;
+    tram = (baso / 100).floor();
+    chuc = ((baso % 100) / 10).floor();
+    donvi = (baso % 10).floor();
     if (tram == 0 && chuc == 0 && donvi == 0) return "";
     if (tram != 0) {
       KetQua += ChuSo[tram] + " trăm ";
@@ -119,92 +121,4 @@ class ConvertUtils {
     }
     return KetQua;
   }
-
-  // String convertPriceToText(String price) {
-  //   String s = price;
-  //   List<String> so = [
-  //     "không",
-  //     "một",
-  //     "hai",
-  //     "ba",
-  //     "bốn",
-  //     "năm",
-  //     "sáu",
-  //     "bảy",
-  //     "tám",
-  //     "chín"
-  //   ];
-  //   List<String> hang = ["", "nghìn", "triệu", "tỷ"];
-  //   int i, j, donvi, chuc, tram;
-  //   String str = " ";
-  //   bool booAm = false;
-  //   int decS = 0;
-
-  //   try {
-  //     decS = int.parse(s.toString());
-  //   } catch (e) {
-  //     print('ERROR IN CONVERT PRICE TO TEXT: $e');
-  //   }
-
-  //   if (decS < 0) {
-  //     decS = -decS;
-  //     s = decS.toString();
-  //     booAm = true;
-  //   }
-
-  //   i = s.length;
-  //   if (i == 0) {
-  //     str = so[0] + str;
-  //   } else {
-  //     j = 0;
-  //     while (i > 0) {
-  //       donvi = int.parse(s.substring(i - 1, 1));
-  //       i--;
-  //       if (i > 0)
-  //         chuc = int.parse(s.substring(i - 1, 1));
-  //       else
-  //         chuc = -1;
-
-  //       i--;
-
-  //       if (i > 0)
-  //         tram = int.parse(s.substring(i - 1, 1));
-  //       else
-  //         tram = -1;
-
-  //       i--;
-
-  //       if ((donvi > 0) || (chuc > 0) || (tram > 0) || (j == 3)) {
-  //         str = hang[j] + str;
-  //       }
-  //       j++;
-  //       if (j > 3) j = 1;
-  //       if ((donvi == 1) && (chuc > 1))
-  //         str = "một " + str;
-  //       else {
-  //         if ((donvi == 5) && (chuc > 0))
-  //           str = "lăm " + str;
-  //         else if (donvi > 0) str = so[donvi] + " " + str;
-  //       }
-
-  //       if (chuc < 0)
-  //         break;
-  //       else {
-  //         if ((chuc == 0) && (donvi > 0)) str = "lẻ " + str;
-  //         if (chuc == 1) str = "mười " + str;
-  //         if (chuc > 1) str = so[chuc] + " mươi " + str;
-  //       }
-
-  //       if (tram < 0)
-  //         break;
-  //       else {
-  //         if ((tram > 0) || (chuc > 0) || (donvi > 0))
-  //           str = so[tram] + " trăm " + str;
-  //       }
-  //       str = " " + str;
-  //     }
-  //   }
-  //   if (booAm) str = "Âm " + str;
-  //   return str + "đồng chẵn";
-  // }
 }
